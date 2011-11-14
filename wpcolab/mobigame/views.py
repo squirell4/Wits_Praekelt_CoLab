c0ecb7a3272e91add62b70cd6e479a15163ef210
Simon Cross <hodgestar@gmail.com>
2011-11-15 01:05:01 +0200

    Skeleton for index, login, select colour and play views.

--- wpcolab/mobigame/views.py
+++ wpcolab/mobigame/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+"""Mobi game views."""
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    return render_to_response('index.html',
+                              context_instance=RequestContext(request))
+
+
+def login(request):
+    return render_to_response('login.html',
+                              context_instance=RequestContext(request))
+
+
+def select_colour(request):
+    pass
+
+
+def play(request):
+    pass
