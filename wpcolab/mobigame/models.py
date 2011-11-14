0b04e454f8ca7aef900b23aeda78fb1dcba57163
Simon Cross <hodgestar@gmail.com>
2011-11-14 23:55:54 +0200

    Basic level, question and answer models.

--- wpcolab/mobigame/models.py
+++ wpcolab/mobigame/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Level(models.Model):
+    """Round of play. Higher levels are more difficult."""
+    levelno = models.IntegerField(primary_key=True,
+                                validators=[MinValueValidator(1)])
+
+    def __unicode__(self):
+        return u"Level %d" % self.levelno
+
+
+class Question(models.Model):
+    text = models.TextField()
+    level = models.ForeignKey(Level)
+
+    def __unicode__(self):
+        return u"%s (%s)" % (self.text, self.level)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    correct = models.BooleanField()
+    question = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return u"%s (%s)" % (self.text,
+                             "correct" if self.correct else "incorrect")
