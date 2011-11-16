6206c9953c85b728b9998570901f7bd3b98b961f
Simon Cross <hodgestar@gmail.com>
2011-11-16 21:56:10 +0200

    Add LoginForm and use it.

--- wpcolab/mobigame/views.py
+++ wpcolab/mobigame/views.py
@@ -1,44 +1,72 @@
 """Mobi game views."""
 
-from django.shortcuts import render_to_response, redirect
-from django.template import RequestContext
+from django.shortcuts import redirect, render
+from django.forms import ModelForm
 
 from mobigame.models import Game, Player
 
 
+class LoginForm(ModelForm):
+    class Meta:
+        model = Player
+        fields = ('first_name', 'colour')
+
+    def clean(self):
+        super(LoginForm, self).clean()
+        if self.colour not in self.game.unused_colours:
+            raise ValueError("Colour already taken, sorry. :(")
+
+
 def index(request):
     game = Game.current_game()
-    if len(game.player_set.all()) < 4:
+    if game.player_set.count() < 4:
         return redirect('login')
     redirect('gamefull')
 
 
 def login(request):
     game = Game.current_game()
-    if len(game.player_set.all()) >= 4:
+    if game.player_set.count() >= 4:
         return redirect('gamefull')
+
+    if request.method == 'POST':
+        player = Player(game=game)
+        login_form = LoginForm(request.POST, instance=player)
+        if login_form.is_valid():
+            player = login_form.save()
+            if game.player_set.count() >= 4:
+                return redirect('getready')
+            return redirect('findafriend')
+    else:
+        login_form = LoginForm()
+
     unused_colours = sorted(list(game.unused_colours()))
-    colour_options = [(colour, Player.COLOUR_DISPLAY[colour])
-                      for colour in unused_colours]
-    context = RequestContext(request, {
+    context = {
         'first_colour_style': Player.COLOUR_STYLES[unused_colours[0]],
-        'colour_options': colour_options,
-        })
-    return render_to_response('login.html',
-                              context_instance=context)
+        'login_form': login_form,
+        }
+    return render(request, 'login.html', context)
 
 
 def signout(request):
-    pass
+    login_form = LoginForm()
+
+    context = {
+        'login_form': login_form,
+        }
+    return render(request, 'signout.html', context)
 
 
 def gamefull(request):
-    pass
+    context = {}
+    return render(request, 'gamefull.html', context)
 
 
 def findafriend(request):
-    pass
+    context = {}
+    return render(request, 'findafriend.html', context)
 
 
 def getready(request):
-    pass
+    context = {}
+    return render(request, 'getready.html', context)
