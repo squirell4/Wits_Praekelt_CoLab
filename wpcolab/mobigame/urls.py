67f6a5e334f5e816d5f11e3739d875cf8b486e2d
Simon Cross <hodgestar@gmail.com>
2011-11-16 22:37:00 +0200

    Use login form, fill in signout template, fix colour validation in LoginForm. Fix references to namespaced views in redirects.

--- wpcolab/mobigame/urls.py
+++ wpcolab/mobigame/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^login/', views.login, name='login'),
     url(r'^signout/', views.signout, name='signout'),
     url(r'^findafriend/', views.findafriend, name='findafriend'),
--- wpcolab/mobigame/views.py
+++ wpcolab/mobigame/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import redirect, render
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 
 from mobigame.models import Game, Player
 
@@ -13,21 +14,31 @@ class LoginForm(ModelForm):
 
     def clean(self):
         super(LoginForm, self).clean()
-        if self.colour not in self.game.unused_colours:
-            raise ValueError("Colour already taken, sorry. :(")
+        if (self.cleaned_data['colour'] not in
+            self.instance.game.unused_colours()):
+            raise ValidationError("Colour already taken, sorry. :(")
+        return self.cleaned_data
+
+
+def first_colour_style(game=None):
+    """Retrieve the style of the first available colour."""
+    if game is None:
+        game = Game.current_game()
+    unused_colours = sorted(list(game.unused_colours()))
+    return Player.COLOUR_STYLES[unused_colours[0]]
 
 
 def index(request):
     game = Game.current_game()
     if game.player_set.count() < 4:
-        return redirect('login')
-    redirect('gamefull')
+        return redirect('mobigame:login')
+    redirect('mobigame:gamefull')
 
 
 def login(request):
     game = Game.current_game()
     if game.player_set.count() >= 4:
-        return redirect('gamefull')
+        return redirect('mobigame:gamefull')
 
     if request.method == 'POST':
         player = Player(game=game)
@@ -35,14 +46,13 @@ def login(request):
         if login_form.is_valid():
             player = login_form.save()
             if game.player_set.count() >= 4:
-                return redirect('getready')
-            return redirect('findafriend')
+                return redirect('mobigame:getready')
+            return redirect('mobigame:findafriend')
     else:
         login_form = LoginForm()
 
-    unused_colours = sorted(list(game.unused_colours()))
     context = {
-        'first_colour_style': Player.COLOUR_STYLES[unused_colours[0]],
+        'first_colour_style': first_colour_style(game),
         'login_form': login_form,
         }
     return render(request, 'login.html', context)
@@ -52,6 +62,7 @@ def signout(request):
     login_form = LoginForm()
 
     context = {
+        'first_colour_style': first_colour_style(),
         'login_form': login_form,
         }
     return render(request, 'signout.html', context)
