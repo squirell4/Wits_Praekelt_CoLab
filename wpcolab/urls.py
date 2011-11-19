19491055213436863681f516c67a2805e954f3f6
Simon Cross <hodgestar@gmail.com>
2011-11-19 19:06:25 +0200

    Add sentry to URLs and clean-up URL file a bit.

--- wpcolab/urls.py
+++ wpcolab/urls.py
@@ -4,14 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wpcolab.views.home', name='home'),
-    # url(r'^wpcolab/', include('wpcolab.foo.urls')),
-
     # mobigame
     url(r'^', include('mobigame.urls', namespace='mobigame')),
 
     # admin site
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include('admin.site.urls')),
+    url(r'^sentry/', include('sentry.web.urls')),
 )
